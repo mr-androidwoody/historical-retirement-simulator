d80e6bdf182a60a47c36b76a629205a30b3a01d5
mr-androidwoody <android.woody@gmail.com>
2026-03-16 11:26:46 +0000

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const worker = new Worker("./js/worker/worker.js?v=2", { type: "module" });
+const worker = new Worker("./js/worker/worker.js?v=3", { type: "module" });
 
 worker.onmessage = (event) => {
   const { ok, result, error } = event.data || {};
