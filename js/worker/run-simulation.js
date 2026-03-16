52cce6c4e7c8345fd9a46921799065527b38aae4
mr-androidwoody <android.woody@gmail.com>
2026-03-16 15:00:51 +0000

    Add files via upload

--- js/worker/run-simulation.js
+++ js/worker/run-simulation.js
@@ -1,13 +1,18 @@
+import { runHistoricalMode } from "./modes/run-historical.js";
+
 export function runSimulationByMode({ mode = "historical", inputs }) {
-  return {
-    scenarios: [],
-    summary: {
-      successRate: 0,
-      medianTerminalWealth: 0,
-      p10TerminalWealth: 0,
-      p90TerminalWealth: 0,
-      scenarioCount: 0,
-      modeUsed: mode
-    }
-  };
+  if (mode !== "historical") {
+    return {
+      scenarios: [],
+      summary: {
+        successRate: 0,
+        medianTerminalWealth: 0,
+        p10TerminalWealth: 0,
+        p90TerminalWealth: 0,
+        scenarioCount: 0
+      }
+    };
+  }
+
+  return runHistoricalMode(inputs);
 }
