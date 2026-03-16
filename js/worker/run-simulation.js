3fe99877ef4b466a3e40b52aab9768c9c92432c8
mr-androidwoody <android.woody@gmail.com>
2026-03-16 15:09:45 +0000

    Add files via upload

--- js/worker/run-simulation.js
+++ js/worker/run-simulation.js
@@ -1,18 +1,19 @@
 import { runHistoricalMode } from "./modes/run-historical.js";
+import { runMonteCarloMode } from "./modes/run-montecarlo.js";
 
 export function runSimulationByMode({ mode = "historical", inputs }) {
-  if (mode !== "historical") {
-    return {
-      scenarios: [],
-      summary: {
-        successRate: 0,
-        medianTerminalWealth: 0,
-        p10TerminalWealth: 0,
-        p90TerminalWealth: 0,
-        scenarioCount: 0
+  return {
+    scenarios: [],
+    summary: {
+      successRate: 0,
+      medianTerminalWealth: 0,
+      p10TerminalWealth: 0,
+      p90TerminalWealth: 0,
+      scenarioCount: 0,
+      test: {
+        historicalLoaded: typeof runHistoricalMode === "function",
+        monteCarloLoaded: typeof runMonteCarloMode === "function"
       }
-    };
-  }
-
-  return runHistoricalMode(inputs);
+    }
+  };
 }
