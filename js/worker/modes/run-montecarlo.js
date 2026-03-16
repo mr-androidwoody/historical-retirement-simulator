040ba4a9b4d2514c4d09550158c2421a21f424e9
mr-androidwoody <android.woody@gmail.com>
2026-03-16 15:13:34 +0000

    Add files via upload

--- js/worker/modes/run-montecarlo.js
+++ js/worker/modes/run-montecarlo.js
@@ -1,7 +1,3 @@
-import { simulateScenario } from "../../model/engine.js";
-import { createMonteCarloReturnsProvider } from "../../model/returns/montecarlo.js";
-import { aggregateResults } from "../../model/analysis/aggregator.js";
-
 export function runMonteCarloMode(inputs) {
   return {
     scenarios: [],
@@ -11,12 +7,7 @@ export function runMonteCarloMode(inputs) {
       p10TerminalWealth: 0,
       p90TerminalWealth: 0,
       scenarioCount: 0,
-      test: {
-        simulateScenarioLoaded: typeof simulateScenario === "function",
-        createMonteCarloReturnsProviderLoaded:
-          typeof createMonteCarloReturnsProvider === "function",
-        aggregateResultsLoaded: typeof aggregateResults === "function"
-      }
+      test: "run-montecarlo-loaded"
     }
   };
 }
