0dec676c72821521c28c75d1fa14b48f5eb1857f
mr-androidwoody <android.woody@gmail.com>
2026-03-16 15:39:06 +0000

    Add files via upload

--- js/worker/run-simulation.js
+++ js/worker/run-simulation.js
@@ -1,19 +1,19 @@
-import { runHistoricalMode } from "./modes/run-historical.js?v=debug9";
-import { runMonteCarloMode } from "./modes/run-montecarlo.js?v=debug9";
+import { runHistoricalMode } from "./modes/run-historical.js";
+import { runMonteCarloMode } from "./modes/run-montecarlo.js";
+import { runDeterministicMode } from "./modes/run-deterministic.js";
 
 export function runSimulationByMode({ mode = "historical", inputs }) {
-  return {
-    scenarios: [],
-    summary: {
-      successRate: 0,
-      medianTerminalWealth: 0,
-      p10TerminalWealth: 0,
-      p90TerminalWealth: 0,
-      scenarioCount: 0,
-      test: {
-        historicalLoaded: typeof runHistoricalMode === "function",
-        monteCarloLoaded: typeof runMonteCarloMode === "function"
-      }
-    }
-  };
+  switch (mode) {
+    case "historical":
+      return runHistoricalMode(inputs);
+
+    case "montecarlo":
+      return runMonteCarloMode(inputs);
+
+    case "deterministic":
+      return runDeterministicMode(inputs);
+
+    default:
+      throw new Error(`Unsupported simulation mode: ${mode}`);
+  }
 }
--- js/worker/worker.js
+++ js/worker/worker.js
@@ -1,4 +1,4 @@
-import { runSimulationByMode } from "./run-simulation.js?v=debug9";
+import { runSimulationByMode } from "./run-simulation.js";
 
 self.onmessage = (event) => {
   const { type, mode = "historical", inputs } = event.data || {};
