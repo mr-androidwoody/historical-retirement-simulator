e5d56489731d3fbe7a00822c58d2c0412adad89c
mr-androidwoody <android.woody@gmail.com>
2026-03-16 08:52:20 +0000

    Add files via upload

--- js/model/engine.js
+++ js/model/engine.js
@@ -1,1 +1,73 @@
+export function simulateScenario({ inputs, returnsProvider }) {
 
+  const {
+    initialPortfolio,
+    annualSpending,
+    years,
+    equityAllocation,
+    bondAllocation,
+    cashAllocation,
+    fees
+  } = inputs;
+
+  let portfolio = initialPortfolio;
+
+  const yearlyRows = [];
+  const pathNominal = [];
+  const pathReal = [];
+
+  let inflationIndex = 1;
+
+  for (let year = 0; year < years; year++) {
+
+    const returns = returnsProvider.getYearReturns(year);
+
+    const startPortfolio = portfolio;
+
+    const withdrawal = annualSpending;
+
+    portfolio -= withdrawal;
+
+    const weightedReturn =
+      equityAllocation * returns.equities +
+      bondAllocation * returns.bonds +
+      cashAllocation * returns.cashlike;
+
+    portfolio = portfolio * (1 + weightedReturn);
+
+    portfolio = portfolio * (1 - fees);
+
+    inflationIndex = inflationIndex * (1 + returns.inflation);
+
+    const realPortfolio = portfolio / inflationIndex;
+
+    yearlyRows.push({
+      year,
+      startPortfolio,
+      withdrawal,
+      portfolioReturn: weightedReturn,
+      endPortfolio: portfolio,
+      inflation: returns.inflation,
+      realPortfolio
+    });
+
+    pathNominal.push(portfolio);
+    pathReal.push(realPortfolio);
+
+    if (portfolio <= 0) {
+      return {
+        yearlyRows,
+        pathNominal,
+        pathReal,
+        depleted: true
+      };
+    }
+  }
+
+  return {
+    yearlyRows,
+    pathNominal,
+    pathReal,
+    depleted: false
+  };
+}
--- js/model/simulator.js
+++ js/model/simulator.js
@@ -1,1 +1,11 @@
+import { simulateScenario } from "./engine.js";
 
+export function runRetirementSimulation({ inputs, returnsProvider }) {
+
+  const result = simulateScenario({
+    inputs,
+    returnsProvider
+  });
+
+  return result;
+}
