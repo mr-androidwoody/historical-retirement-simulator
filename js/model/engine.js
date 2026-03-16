f43863d3cc4a8ed656681103f82bc49abc320728
mr-androidwoody <android.woody@gmail.com>
2026-03-16 16:15:26 +0000

    Add files via upload

--- js/model/engine.js
+++ js/model/engine.js
@@ -1,14 +1,21 @@
 export function simulateScenario({ inputs, returnsProvider }) {
+  const initialPortfolio = Number(
+    inputs.startingPortfolio ?? inputs.initialPortfolio ?? 0
+  );
 
-  const initialPortfolio = Number(inputs.initialPortfolio ?? 0);
   const annualSpending = Number(inputs.annualSpending ?? 0);
-  const years = Number(inputs.years ?? 0);
+
+  const years = Number(
+    inputs.simulationYears ?? inputs.years ?? 0
+  );
 
   const equityAllocation = Number(inputs.equityAllocation ?? 0);
   const bondAllocation = Number(inputs.bondAllocation ?? 0);
   const cashAllocation = Number(inputs.cashAllocation ?? 0);
 
-  const fees = Number(inputs.fees ?? 0);
+  const fees = Number(
+    inputs.annualFees ?? inputs.fees ?? 0
+  );
 
   let portfolio = initialPortfolio;
 
@@ -18,12 +25,10 @@ export function simulateScenario({ inputs, returnsProvider }) {
 
   let inflationIndex = 1;
 
-  for (let year = 0; year < years; year++) {
-
+  for (let year = 0; year < years; year += 1) {
     const returns = returnsProvider.getYearReturns(year);
 
     const startPortfolio = portfolio;
-
     const withdrawal = annualSpending;
 
     portfolio -= withdrawal;
@@ -34,7 +39,6 @@ export function simulateScenario({ inputs, returnsProvider }) {
       cashAllocation * returns.cashlike;
 
     portfolio = portfolio * (1 + weightedReturn);
-
     portfolio = portfolio * (1 - fees);
 
     inflationIndex = inflationIndex * (1 + returns.inflation);
@@ -59,6 +63,8 @@ export function simulateScenario({ inputs, returnsProvider }) {
         yearlyRows,
         pathNominal,
         pathReal,
+        terminalNominal: portfolio,
+        terminalReal: realPortfolio,
         depleted: true
       };
     }
@@ -68,6 +74,8 @@ export function simulateScenario({ inputs, returnsProvider }) {
     yearlyRows,
     pathNominal,
     pathReal,
+    terminalNominal: pathNominal[pathNominal.length - 1] ?? initialPortfolio,
+    terminalReal: pathReal[pathReal.length - 1] ?? initialPortfolio,
     depleted: false
   };
 }
