33162f8258f874a19aa4058b0d171fa3353ceeb7
mr-androidwoody <android.woody@gmail.com>
2026-03-16 14:33:49 +0000

    Add files via upload

--- js/model/simulator.js
+++ js/model/simulator.js
@@ -1,11 +1,41 @@
 import { simulateScenario } from "./engine.js";
+import { aggregateResults } from "./analysis/aggregator.js";
+import { createHistoricalReturnsProvider } from "./returns/historical.js";
 
-export function runRetirementSimulation({ inputs, returnsProvider }) {
+export function runRetirementSimulation(inputs) {
+  const scenarios = [];
 
-  const result = simulateScenario({
-    inputs,
-    returnsProvider
-  });
+  const horizon = inputs.simulationYears;
+  const dataset = inputs.dataset;
 
-  return result;
+  const series = dataset.composites.gdpWeighted.series;
+  const datasetLength = series.length;
+
+  const windowCount = datasetLength - horizon + 1;
+
+  for (let startIndex = 0; startIndex < windowCount; startIndex++) {
+    const endIndex = startIndex + horizon;
+
+    const window = series.slice(startIndex, endIndex);
+
+    const returnsProvider = createHistoricalReturnsProvider(window);
+
+    const scenario = simulateScenario({
+      inputs,
+      returnsProvider
+    });
+
+    scenarios.push({
+      startYear: window[0].year,
+      endYear: window[window.length - 1].year,
+      ...scenario
+    });
+  }
+
+  const summary = aggregateResults(scenarios);
+
+  return {
+    scenarios,
+    summary
+  };
 }
